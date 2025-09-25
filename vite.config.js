0df4734c1380912e49f0d301343e1f78f7c1a304
JBibu <javims@jbibu.xyz>
2025-09-25 15:45:56 +0200

    Changed config

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(() => {
     test: {
       globals: true,
       environment: "jsdom",
+      testTimeout: 10000,
       coverage: {
         exclude: ["**/build/**", "src/setupProxy.js", ...coverageConfigDefaults.exclude],
       },
